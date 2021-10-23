62edab49ee248b67b72770b476c58e4017326d4b
= <sartakov.sasha@icloud.com>
2021-10-23 21:07:07 +0700

    task

--- task_13_8_19.py
+++ task_13_8_19.py
@@ -1,19 +1,21 @@
-cnt = int(input('How many tickets do you wanna buy?\n'))
-youth = adult = 0
-for i in range (cnt):
-    age = int(input("What is the age of visitor?"))
-    if age < 0:
-        print("You're not born yeat ;)")
-    elif age < 18:
-        continue
-    elif 18 < age < 25:
-        youth += 1
-    else:
-        adult += 1
+def main():
+    cnt = int(input('How many tickets do you wanna buy?\n'))
+    youth = adult = 0
 
-discount = 1 if cnt < 3 else 0.9
-result = float ((youth * 990 + adult * 1390) * discount)
-if result:
-    print(f"You have to pay O_o \n{result} ₽")
-else:
-    print("You don't have to pay =)")
+    try:
+        ages = list(map(int, input("What are the ages of the visitors?\n").split()))
+        #ages = [int(input()) for i in range (cnt)]
+    except ValueError as e:
+        print("It's a strange age...")
+
+    for age in ages:
+        if age > 25:
+            adult += 1
+        elif age >= 18:
+            youth += 1
+
+    result = float ((youth * 990 + adult * 1390) * (0.9 if cnt > 3 else 1))
+    print(f"You have to pay O_o\n{result} ₽" if result else "You don't have to pay =)")
+
+if __name__=="__main__":
+    main()
